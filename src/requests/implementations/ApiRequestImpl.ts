0bc2316b29ed6139543b54efce368e189fe4f426
hyptexxx <ilya-2379@yandex.ru>
2020-12-04 04:37:56 +0300

    fix

--- src/requests/implementations/ApiRequestImpl.ts
+++ src/requests/implementations/ApiRequestImpl.ts
@@ -64,6 +64,6 @@ export default class ApiRequestImpl extends Vue implements ApiRequestService {
   }
 
   public async deleteEmployeeRequest (employeeId: number[]): Promise<void> {
-    await this.$axios.delete('/employee/1')
+    await this.$axios.delete(`/employee/${employeeId.toString()}`)
   }
 }
